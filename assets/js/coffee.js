18bd4d6fe8191bf398c7c13cf5102a28f091d43d
hykim497 <hy.kim@berkeley.edu>
2016-04-27 16:04:35 -0700

    coffee update

--- assets/js/coffee.js
+++ assets/js/coffee.js
@@ -1,8 +1,27 @@
 $(document).ready(function(){
 
 $("#arashiyama").hover(function() {
-	$("#flower").FadeIn("slow");
-	$("#flower").FadeOut("slow");
+	$("#flower").show(); 
+	}, function () {
+	$("#flower").hide();
+});
+
+$("#oyheart").hover(function() {
+	$("#heart").show(); 
+	}, function () {
+	$("#heart").hide();
+});
+
+$("#dream").hover(function() {
+	$("#rocket").show(); 
+	}, function () {
+	$("#rocket").hide();
+});
+
+$("#guat").hover(function() {
+	$("#tree").show(); 
+	}, function () {
+	$("#tree").hide();
 });
 
 $("#pagewrapper").click(function() {
